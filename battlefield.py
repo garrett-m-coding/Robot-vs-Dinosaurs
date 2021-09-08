f9bf7edc5356e33b9256e9bd4414996478380c42
garrett-m-coding <garrett.mcginnis.coding@gmail.com>
2021-09-08 16:06:44 -0500

    Created files for each class I need to create. I finished the shortest class 'Weapon'.

---
+++ battlefield.py
@@ -0,0 +1,35 @@
+class Battlefield:
+    def __init__(self, name):
+        self.name = name
+        pass
+
+    def greeting(self):
+        print('Welcome to Dinos vs Robots Battlefield')
+        pass
+
+        
+        
+        
+        
+        
+        
+    #     self.run_game = ''
+    #     self.display_welcome = ''
+    #     self.battle = ''
+        
+    # def    
+    # self.dino_turn = ''
+    # self.robo_turn = ''
+
+
+    # def set_current_time(self):
+    #     self.current_time = input("Enter the current time: ")
+    #     print("Alarm clock current time: ", self.current_time)
+
+    # def set_alarm_on_off(self):
+    #     self.alarm_on_off = input("Enter 'on' or 'off' to turn on/off the alarm.").lower()
+    #     print("Alarm is now ", self.alarm_on_off, " don't forget to set alarm time.")
+
+    # def set_alarm_time(self):
+    #     self.alarm_time = input("Enter alarm time (i.e. - '7:30 AM').")
+    #     print("Alarm clock alarm time: ", self.alarm_time)
---
+++ main.py
@@ -0,0 +1,4 @@
+from battlefield import Battlefield
+
+blue_battlefield = Battlefield('blue')
+blue_battlefield.greeting()
